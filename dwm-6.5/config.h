53b1bc836f227977e0ffe5ddf4d50734e40c4e18
cliff <cliffabbio@gmail.com>
2025-03-20 15:54:45 -0400

    fixed dwm icons

--- dwm-6.5/config.h
+++ dwm-6.5/config.h
@@ -11,7 +11,7 @@ static const unsigned int gappx     = 20;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Libertinus:size=9", "JoyPixels:pixelsize=20:antialias=true:autohint=true" };
+static const char *fonts[]          = { "Libertinus:size=9", "Noto Color Emoji:pixelsize=20:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
